8113cd345f6b7c95695d1a3d56f1bc22c1376ff8
Aaron Clark (Arcus) <aclark02@arcusbio.com>
2024-03-28 10:07:30 -0400

    fix issue with dfs

--- app.R
+++ app.R
@@ -1,22 +1,26 @@
 
-install.packages("forestly")
-install.packages("metalite")
+# install.packages("forestly")
+# install.packages("remotes")
+# remotes::install_github("Merck/forestly")
+# install.packages("metalite")
+# rsconnect::writeManifest()
 
 library(metalite)
 library(forestly)
 
-forestly_adsl$TRTA <- factor(forestly_adsl$TRT01A, levels = c("Xanomeline Low Dose", "Placebo"), labels = c("Low Dose", "Placebo"))
-
-forestly_adae$TRTA <- factor(forestly_adae$TRTA, levels = c("Xanomeline Low Dose", "Placebo"), labels = c("Low Dose", "Placebo"))
-
+adsl <- forestly::forestly_adsl
+adae <- forestly::forestly_adae
+adsl$TRTA <- factor(adsl$TRT01A, levels = c("Xanomeline Low Dose", "Placebo"), labels = c("Low Dose", "Placebo"))
+adae$TRTA <- factor(adae$TRTA, levels = c("Xanomeline Low Dose", "Placebo"), labels = c("Low Dose", "Placebo"))
 
 
 meta_forestly(
-  forestly_adsl,
-  forestly_adae,
+  adsl,
+  adae,
   population_term = "apat",
   observation_term = "wk12"
 ) |>
   prepare_ae_forestly(parameter = "any;rel;ser") |>
   format_ae_forestly() |>
   ae_forestly()
+
